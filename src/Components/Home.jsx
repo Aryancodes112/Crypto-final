24759e6e6720a5562b18cc87696342fc20f53040
Aryan <aryanykumbhar@gmail.com>
2024-08-08 22:54:53 +0530

    fix:framer verson

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import btcSrc from "../assets/btc.png";
-import { motion } from 'framer-motion/dist/framer-motion'
+import { motion } from "framer-motion"
 
 const Home = () => {
   return (
